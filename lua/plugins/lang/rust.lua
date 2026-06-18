b6e2d3eb52dd6f1cc859ac73878d89d5e31934df
cradiy <cradiy.rs@gmail.com>
2026-06-18 16:07:08 +0800

    disable workspace check

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -1,48 +1,59 @@
 vim.g.rustaceanvim = {
-	-- Plugin configuration
-	tools = {},
-	-- LSP configuration
-	server = {
-		on_attach = function(client, bufnr)
-			-- you can also put keymaps in here
-		end,
-		default_settings = {
-			-- rust-analyzer language server configuration
-			["rust-analyzer"] = {
-				inlayHints = {
-					enabled = true,
-				},
-				procMacro = {
-					enable = true,
-				},
-			},
-		},
-	},
-	-- DAP configuration
-	dap = {},
+  -- Plugin configuration
+  tools = {},
+  -- LSP configuration
+  server = {
+    on_attach = function()
+      -- you can also put keymaps in here
+    end,
+    default_settings = {
+      -- rust-analyzer language server configuration
+      ["rust-analyzer"] = {
+        inlayHints = {
+          enabled = true,
+        },
+        -- cachePriming = {
+        -- 	enable = false,
+        -- },
+        procMacro = {
+          enable = true,
+        },
+        check = {
+          command = "clippy",
+          extraArgs = {
+            "--no-deps",
+            "--message-format=json-diagnostic-rendered-ansi",
+          },
+          workspace = false,
+        },
+      },
+    },
+  },
+  -- DAP configuration
+  dap = {},
 }
 
 return {
-	{
-		"Saecki/crates.nvim",
-		event = { "BufRead Cargo.toml" },
-		opts = {
-			completion = {
-				crates = {
-					enabled = true,
-				},
-			},
-			lsp = {
-				enabled = true,
-				actions = true,
-				completion = true,
-				hover = true,
-			},
-		},
-	},
-	{
-		"mrcjkb/rustaceanvim",
-		version = "*", -- Recommended
-		lazy = false, -- This plugin is already lazy
-	},
+  {
+    "Saecki/crates.nvim",
+    event = { "BufRead Cargo.toml" },
+    opts = {
+      completion = {
+        crates = {
+          enabled = true,
+        },
+      },
+      lsp = {
+        enabled = true,
+        actions = true,
+        completion = true,
+        hover = true,
+      },
+    },
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "*", -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
 }
